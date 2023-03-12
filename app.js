71ac958bbc4ce8bb924dd53fc7c0372b7fbb6d87
meshimee <simiaktar527@gmail.com>
2023-03-12 19:33:25 +0530

    new

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const tasks = require('./routes/tasks.js');
 const connectDB = require('./db/connect.js');
 require('dotenv').config();
+const http = require("http");
+const server = http.createServer(app);
 //middleware
 app.use(express.static('./public'));
 app.use(express.json());
@@ -13,7 +15,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
     try {
       await connectDB(process.env.MONGO_URI);
-      app.listen(port, () =>
+      server.listen(port, () =>
         console.log(`Server is listening on port ${port}...`)
       );
     } catch (error) {
