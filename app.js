cf104a7ad928fd224775046dff0fcaad49ddfa5f
meshimee <simiaktar527@gmail.com>
2023-03-12 19:11:41 +0530

    new

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('./public'));
 app.use(express.json());
 
 app.use('/api/v1/tasks',tasks);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
